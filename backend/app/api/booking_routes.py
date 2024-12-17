1c73b8599b85810128430c83fb4325a088e94d46
Togar-GitHub <togar_unix@Togar-Laptop>
2024-12-17 13:54:35 -0800

    all routes of booking

--- backend/app/api/booking_routes.py
+++ backend/app/api/booking_routes.py
@@ -1,19 +1,37 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from ..models import db, Booking, Service
 from ..forms import BookingForm
 
 booking_routes = Blueprint('bookings', __name__)
 
-@booking_routes.route('/')
+@booking_routes.route('/', methods=['GET'])
 def get_booking():
   bookings = Booking.query.all()
   return {'bookings': [booking.to_dict() for booking in bookings]}
 
 
+@booking_routes.route('/<int:id>', methods=['GET'])
+def get_booking_one(id):
+    booking = Booking.query.get(id)
+    if booking is None:
+      return jsonify({'message': 'Booking not found'}),
+    return jsonify({'booking': booking.to_dict()})
+
+
 @booking_routes.route('/', methods=['POST'])
 def create_booking():
   form = BookingForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  form.services.choices = [(service.id) for service in Service.query.all()]
+
   if form.validate_on_submit():
+
+    service_ids = form.services.data
+
+    if service_ids:
+      services = Service.query.filter(Service.id.in_(service_ids)).all()
+
     new_booking = Booking(
       client_id=form.client_id.data,
       pet_id=form.pet_id.data,
@@ -22,9 +40,6 @@ def create_booking():
       pick_up_date=form.pick_up_date.data,
       cost=form.cost.data,
     )
-
-    service_ids=form.services.data
-    services=Service.query.filter(Service.id.in_(service_ids)).all()
 
     new_booking.services.extend(services)
 
@@ -35,3 +50,52 @@ def create_booking():
   
   return jsonify({"errors": form.errors}), 400
 
+
+@booking_routes.route('/<int:id>', methods=['PUT'])
+def update_booking(id):
+  form = BookingForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  form.services.choices = [(service.id) for service in Service.query.all()]
+
+  if form.validate_on_submit():
+
+    service_ids = form.services.data
+
+    if service_ids:
+      services = Service.query.filter(Service.id.in_(service_ids)).all()
+
+    booking = Booking.query.get(id)
+    if not booking:
+      return jsonify({"error": "Booking not found"}), 404
+    
+    booking.client_id = form.client_id.data
+    booking.pet_id = form.pet_id.data
+    booking.booking_type = form.booking_type.data
+    booking.drop_off_date = form.drop_off_date.data
+    booking.pick_up_date = form.pick_up_date.data
+    booking.cost = form.cost.data
+
+    for service in booking.services:
+      booking.services.remove(service)
+
+    booking.services.extend(services)
+
+    db.session.commit()
+
+    return jsonify({"message": "Booking updated successfully!", "booking": booking.to_dict()}), 201
+  
+  return jsonify({"errors": form.errors}), 400
+
+
+@booking_routes.route('/<int:id>', methods=['DELETE'])
+def delete_booking(id):
+  booking = Booking.query.get(id)
+
+  if not booking:
+    return jsonify({"error": "Booking not found"}), 404
+  
+  db.session.delete(booking)
+  db.session.commit()
+
+  return jsonify({"message": "Booking deleted successfully"}), 200
