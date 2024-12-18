2c83e46e662a1b6e22456c650bdd40bc144bbe38
Luke Sauls <luke.sauls279@gmail.com>
2024-12-18 14:17:05 -0700

    chenged docker

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -14,7 +14,8 @@ from .api.pet_routes import pet_routes
 from .seeds import seed_commands
 from .config import Config
 
-app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
+
+app = Flask(__name__, static_folder='.../frontend/dist', static_url_path='/')
 
 # Setup login manager
 login = LoginManager(app)
