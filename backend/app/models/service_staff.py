c7e8f0c0b02267df16ccd8b47a873cba87693303
Luke Sauls <luke.sauls279@gmail.com>
2024-12-18 09:56:08 -0700

    edited seeder order

--- backend/app/models/service_staff.py
+++ backend/app/models/service_staff.py
@@ -2,8 +2,8 @@ from .db import db, environment, SCHEMA
 
 service_staff = db.Table(
     'service_staff',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-    db.Column('service_id', db.Integer, db.ForeignKey('services.id'), primary_key=True)
+    db.Column('user_id', db.Integer, db.ForeignKey(f'{SCHEMA}.users.id' if environment == "production" else 'users.id'), primary_key=True),
+    db.Column('service_id', db.Integer, db.ForeignKey(f'{SCHEMA}.services.id' if environment == "production" else 'services.id'), primary_key=True)
 )
 
 if environment == "production":
