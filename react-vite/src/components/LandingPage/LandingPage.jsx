6e91385a7e10f67696c2d91e71690e59748d2549
glitterpillz <karenhickey1010@gmail.com>
2024-12-18 18:42:08 -0500

    added landing page and removed login and signup page components

---
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import lan from "./LandingPage.module.css"
+
+
+function LandingPage() {
+  return (
+    <div>
+        <img className={lan.logo} src="/images/logo2.webp" alt="logo" />
+    </div>
+  );
+}
+
+export default LandingPage;
