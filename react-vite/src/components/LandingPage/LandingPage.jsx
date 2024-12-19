a57116181ece5ff07f24311f0b62922f053af921
glitterpillz <karenhickey1010@gmail.com>
2024-12-18 23:12:23 -0500

    added layout and styling to landing page

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -7,9 +7,25 @@ function LandingPage() {
     <div className={lan.landingPageContainer}>
         <div className={lan.bannerContainer}>
             <img className={lan.logo} src="/images/logo-banner.jpg" alt="logo" />
+            <div className={lan.profileButton}>
+                <ProfileButton />
+            </div>
         </div>
-        <div className={lan.profileButton}>
-            <ProfileButton />
+        <div className={lan.landingBodyContainer}>
+            <div className={lan.messageContainer}>
+                <img className={lan.pawLeft} src="/images/paw-left.png" alt="paw" />
+                <h1 className={lan.messageBox}>YOUR PET&apos;S ULTIMATE VACATION AWAITS!</h1>
+                <img className={lan.pawRight} src="/images/paw-right.png" alt="paw" />
+            </div>
+
+            <div className={lan.infoContainer}>
+                <div className={lan.aboutBox}>
+                    <h1>Welcome / About Us</h1>
+                </div>
+                <div className={lan.featuresBox}>
+                    <h1>Features Buttons</h1>
+                </div>
+            </div>
         </div>
     </div>
   );
