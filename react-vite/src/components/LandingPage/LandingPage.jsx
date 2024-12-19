9684d65bdd565b054b21c2ada52692af50e26620
glitterpillz <karenhickey1010@gmail.com>
2024-12-18 20:42:50 -0500

    added banner and profile button to landing page

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -1,10 +1,16 @@
+import ProfileButton from "../Navigation/ProfileButton";
 import lan from "./LandingPage.module.css"
 
 
 function LandingPage() {
   return (
-    <div>
-        <img className={lan.logo} src="/images/logo2.webp" alt="logo" />
+    <div className={lan.landingPageContainer}>
+        <div className={lan.bannerContainer}>
+            <img className={lan.logo} src="/images/logo-banner.jpg" alt="logo" />
+        </div>
+        <div className={lan.profileButton}>
+            <ProfileButton />
+        </div>
     </div>
   );
 }
--- react-vite/src/components/Navigation/ProfileButton.jsx
+++ react-vite/src/components/Navigation/ProfileButton.jsx
@@ -1,10 +1,12 @@
+import './ProfileButton.css'
 import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { FaUserCircle } from 'react-icons/fa';
+import { IoPawOutline } from "react-icons/io5";
 import { thunkLogout } from "../../redux/session";
 import OpenModalMenuItem from "./OpenModalMenuItem";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
+
 
 function ProfileButton() {
   const dispatch = useDispatch();
@@ -13,7 +15,7 @@ function ProfileButton() {
   const ulRef = useRef();
 
   const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+    e.stopPropagation();
     setShowMenu(!showMenu);
   };
 
@@ -39,13 +41,15 @@ function ProfileButton() {
     closeMenu();
   };
 
+  const pawIcon = <IoPawOutline className="profile-icon" />
+
   return (
     <>
       <button onClick={toggleMenu}>
-        <FaUserCircle />
+        {pawIcon}
       </button>
       {showMenu && (
-        <ul className={"profile-dropdown"} ref={ulRef}>
+        <ul className="profile-dropdown" ref={ulRef}>
           {user ? (
             <>
               <li>{user.username}</li>
