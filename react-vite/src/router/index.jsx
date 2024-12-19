dadeab393fd03b5cd4d08ea452fc7a2153a1139a
awright222 <alex.gr.wright@gmail.com>
2024-12-19 12:16:19 -0700

    index updates

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -3,6 +3,8 @@ import Layout from "./Layout";
 import LandingPage from "../components/LandingPage";
 import ServicesPage from "../components/ServicesPage";
 import ReviewsPage from "../components/ReviewsPage";
+import { UserPetList, UserPetDetail } from "../components/UserPets";
+import { StaffPetList, StaffPetDetail } from "../components/StaffPets";
 
 export const router = createBrowserRouter([
   {
@@ -18,8 +20,28 @@ export const router = createBrowserRouter([
       },
       {
         path: "reviews",
-        element: <ReviewsPage />
-      }
+        element: <ReviewsPage />,
+      },
+      {
+        path: "pets",
+        element: <UserPetList />,
+        children: [
+          {
+            path: ":petId",
+            element: <UserPetDetail />,
+          },
+        ],
+      },
+      {
+        path: "staff/pets",
+        element: <StaffPetList />,
+        children: [
+          {
+            path: ":petId",
+            element: <StaffPetDetail />,
+          },
+        ],
+      },
     ],
   },
 ]);
