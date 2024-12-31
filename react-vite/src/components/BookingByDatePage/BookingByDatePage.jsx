5fa3cd11008773f5520fea5cdf56811d7a9da3c3
Luke Sauls <luke.sauls279@gmail.com>
2024-12-31 08:00:14 -0700

    fixed centering of words

--- react-vite/src/components/BookingByDatePage/BookingByDatePage.jsx
+++ react-vite/src/components/BookingByDatePage/BookingByDatePage.jsx
@@ -1,58 +1,58 @@
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import Calendar from 'react-calendar';
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Calendar from "react-calendar";
 import * as bookingActions from "../../redux/booking";
-import { fetchAllPets } from '../../redux/pets';
-import 'react-calendar/dist/Calendar.css';
-import bdt from './BookingByDatePage.module.css'
+import { fetchAllPets } from "../../redux/pets";
+import "react-calendar/dist/Calendar.css";
+import bdt from "./BookingByDatePage.module.css";
 
 const BookingByDatePage = () => {
   const dispatch = useDispatch();
-  const bookings = useSelector((state) => state.booking.bookings)
-  const pets = useSelector((state) => state.pets.pets)
-  const [selectedDate, setSelectedDate] = useState(null)
-  const [calendarDate, setCalendarDate] = useState(null)
-  console.log('pets > ', pets)
+  const bookings = useSelector((state) => state.booking.bookings);
+  const pets = useSelector((state) => state.pets.pets);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [calendarDate, setCalendarDate] = useState(null);
+  console.log("pets > ", pets);
 
   const today = new Date();
   today.setHours(0, 0, 0, 0);
 
   useEffect(() => {
     setSelectedDate(getFormattedDate(today));
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (selectedDate) {
-      dispatch(bookingActions.getBookingsByDate(selectedDate))
-      dispatch(fetchAllPets())
+      dispatch(bookingActions.getBookingsByDate(selectedDate));
+      dispatch(fetchAllPets());
     }
-  }, [selectedDate, dispatch])
+  }, [selectedDate, dispatch]);
 
   function getFormattedDate(date) {
-    date.setHours(0, 0, 0, 0)
+    date.setHours(0, 0, 0, 0);
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // months are 0-indexed
-    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // months are 0-indexed
+    const day = String(date.getDate()).padStart(2, "0");
     return `${year}-${month}-${day}`;
   }
 
   const handleDateSelection = (date) => {
     setCalendarDate(date);
-    setSelectedDate(getFormattedDate(date))
-  }
+    setSelectedDate(getFormattedDate(date));
+  };
 
   const tileClassName = ({ date, view }) => {
-    if (view === 'month') {
+    if (view === "month") {
       if (date < today) {
         return bdt.pastDate; // disabling past dates
       }
     }
-  }
+  };
 
   // if (!bookings || !bookings.bookings) {
   //   return <div>Loading...</div>
   // }
-  
+
   return (
     <div className={bdt.mainContainer}>
       <h1 className={bdt.h1}>Services by Date</h1>
@@ -73,15 +73,12 @@ const BookingByDatePage = () => {
 
       {bookings && bookings.length > 0 && (
         <div className={bdt.serviceByDateContainer}>
-
           <div className={bdt.listOfPets}>
             {bookings.map((book) => {
-              const petName = pets[book.pet_id - 1]?.name
+              const petName = pets[book.pet_id - 1]?.name;
               return (
                 <div key={book.id}>
-                  <p className={bdt.serviceByDateDetail}>
-                    Pet Name: {petName}
-                  </p>
+                  <p className={bdt.serviceByDateDetail}>Pet Name: {petName}</p>
                   <p className={bdt.serviceByDateDetail}>
                     Booking Type: {book.booking_type}
                   </p>
@@ -92,21 +89,19 @@ const BookingByDatePage = () => {
                     Pick Up Date and Time: {book.pick_up_date}
                   </p>
                   <p>List of Services:</p>
-                    {book.services.map((serve) => (
-                      <p key={serve.id}>
-                        <p className={bdt.serviceByDateDetail}>{serve.service}</p>
-                      </p>
-                    ))}
-                </div>       
-              )
-
+                  {book.services.map((serve) => (
+                    <p key={serve.id}>
+                      <p className={bdt.serviceByDateDetail}>{serve.service}</p>
+                    </p>
+                  ))}
+                </div>
+              );
             })}
           </div>
-
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default BookingByDatePage
+export default BookingByDatePage;
